0e6a268e4105a722239a69b119753533286c2fe7
Jordan H <jholden.hhnet@gmail.com>
2025-02-19 20:11:57 -0400

    fix favicon

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,15 @@
 
-
-
+import { Metadata } from "next/types";
 import Home from "./home/page";
-import RootLayout from "./layout";
 
+
+export const metadata: Metadata = {
+  title: 'Home Schedule',
+  description: 'Manage your home schedule efficiently',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default function Main() {
   return (
--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import RootLayout from '../layout';
 
 const Profile: React.FC = () => {
   return (
