332adb8746b79258c3624a62e305fd15308628f9
Jordan H <jholden.hhnet@gmail.com>
2025-02-19 20:11:24 -0400

    fix favicon

---
+++ client/src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataFetcher from "@/components/DataFetcher/DataFetcher";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <DataFetcher />
+    </div>
+  );
+};
+
+export default Home;
